478c6b862512d669948ae302bf7c1e7941d450fb
User2000 <frejusbouraima79@gmail.com>
2026-02-12 21:18:47 +0100

    feat: add all widgets to dashboard

--- app/Filament/Pages/DefaultDashboard.php
+++ app/Filament/Pages/DefaultDashboard.php
@@ -2,9 +2,18 @@
 
 namespace App\Filament\Pages;
 
-use App\Filament\Widgets\StatsOverview;
+use App\Filament\Widgets\AttendanceChartWidget;
+use App\Filament\Widgets\HRStatsWidget;
+use App\Filament\Widgets\OrdersStatsWidget;
+use App\Filament\Widgets\QuickActionsWidget;
+use App\Filament\Widgets\QuotesChartWidget;
 use App\Filament\Widgets\SalesChart;
+use App\Filament\Widgets\StatsOverview;
 use App\Filament\Widgets\StockAlert;
+use App\Filament\Widgets\UrssafOverviewWidget;
+use App\Filament\Widgets\VatSummaryWidget;
+use App\Filament\Widgets\WarehouseOverview;
+use App\Filament\Widgets\WarehouseStockSummary;
 use Filament\Pages\Dashboard as BaseDashboard;
 
 class DefaultDashboard extends BaseDashboard
@@ -17,10 +26,27 @@ class DefaultDashboard extends BaseDashboard
     public function getWidgets(): array
     {
         return [
+            // Statistiques principales
             StatsOverview::class,
-            \App\Filament\Widgets\UrssafOverviewWidget::class,
+            QuickActionsWidget::class,
+            
+            // Ventes & Devis
             SalesChart::class,
+            OrdersStatsWidget::class,
+            QuotesChartWidget::class,
+            
+            // Stock
             StockAlert::class,
+            WarehouseOverview::class,
+            WarehouseStockSummary::class,
+            
+            // RH
+            HRStatsWidget::class,
+            AttendanceChartWidget::class,
+            
+            // Comptabilité
+            UrssafOverviewWidget::class,
+            VatSummaryWidget::class,
         ];
     }
 } 
